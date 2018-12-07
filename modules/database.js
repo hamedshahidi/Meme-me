cdae9ab9cbbce7ae3e2af437553790a1e578b6d0
Harrisonnguyen1210 <giahungftu2@gmail.com>
2018-12-07 17:31:48 +0200

    "Small commit"

--- modules/database.js
+++ modules/database.js
@@ -7,6 +7,7 @@ const connect = () => {
         host: process.env.DB_HOST,
         user: process.env.DB_USER,
         database: process.env.DB_DATABASE,
+        password: process.env.DB_PWD
     });
 };
 
